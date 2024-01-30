2032c3b355870c478bf5f597254a615dc2fd7d7f
Baymurat785 <abdimuratovbaymurat03@gmail.com>
2024-01-30 20:09:54 +0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Baymurat Abdumuratov on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
